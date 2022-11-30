e13ebd002c3b7f9f59252f7292a053dce87ee067
Kelly Dwan <ryelle@users.noreply.github.com>
2022-11-30 12:51:00 -0500

    Patterns: Add footer content pattern, use on all pages

---
+++ source/wp-content/themes/wporg-documentation-2022/patterns/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Footer Content
+ * Slug: wporg-documentation-2022/footer-content
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"charcoal-2","textColor":"white","layout":{"type":"constrained","wideSize":"1320px"}} -->
+<div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"fontSize":"heading-1"} -->
+<h2 class="has-heading-1-font-size">Get more help</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Can't find what you're looking for? Find out if others share your experience.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:list {"className":"is-style-links-list"} -->
+<ul class="is-style-links-list"><!-- wp:list-item -->
+<li><a href="#">Ask questions in the support forums ↗</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#">Check out the developer documentation ↗</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
