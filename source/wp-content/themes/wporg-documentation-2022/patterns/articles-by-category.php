d7ed8d1f836364da5dbeddfac5b4e2168ab40700
Kelly Dwan <ryelle@users.noreply.github.com>
2022-12-05 18:43:22 -0500

    Add special Landing Page template and patterns to create topic landing pages
    See #4

---
+++ source/wp-content/themes/wporg-documentation-2022/patterns/articles-by-category.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Articles by Category
+ * Slug: wporg-documentation-2022/articles-by-category
+ * Category: query
+ */
+
+?>
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|light-grey-1","style":"solid","width":"1px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-bottom-color:var(--wp--preset--color--light-grey-1);border-bottom-style:solid;border-bottom-width:1px"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<h2 style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Installation</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:query {"queryId":29,"query":{"perPage":"10","pages":0,"offset":0,"postType":"helphub_article","order":"asc","orderBy":"title","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"taxQuery":{"category":[19]}},"displayLayout":{"type":"list","columns":2}} -->
+<div class="wp-block-query"><!-- wp:post-template -->
+<!-- wp:post-title {"level":3,"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|blueberry-1"}}}},"textColor":"blueberry-1","fontSize":"normal","fontFamily":"inter"} /-->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
+
+<!-- wp:paragraph -->
+<p><a href="https://wordpress.org/support/category/installation/">Show all articles in Installation</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
---
+++ source/wp-content/themes/wporg-documentation-2022/patterns/page-header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Page Header
+ * Slug: wporg-documentation-2022/page-header
+ * Category: headers
+ */
+
+?>
+<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:heading {"level":1} -->
+<h1>Technical Guides</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"8px"}}},"textColor":"charcoal-4"} -->
+<p class="has-charcoal-4-color has-text-color" style="margin-top:8px">Learn how to install WordPress and keep it secure and healthy</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search the documentation","widthUnit":"px","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"borderColor":"light-grey-1","className":"is-style-secondary-search-control"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
