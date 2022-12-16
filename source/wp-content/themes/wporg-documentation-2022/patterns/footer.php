4d14786b32b37685a3e7613b3aa160e41f85c476
Kelly Dwan <ryelle@users.noreply.github.com>
2022-12-16 15:41:23 -0500

    Layout: Update left and right spacing to use new preset, center align all pages on large screens
    See https://github.com/WordPress/wporg-parent-2021/issues/65.
    Fixes #15

--- source/wp-content/themes/wporg-documentation-2022/patterns/footer.php
+++ source/wp-content/themes/wporg-documentation-2022/patterns/footer.php
@@ -6,11 +6,10 @@
  */
 
 ?>
-
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"charcoal-2","textColor":"white","layout":{"type":"constrained","wideSize":"1320px"}} -->
-<div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
-<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"fontSize":"heading-1"} -->
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"5rem","left":"var:preset|spacing|edge-space","bottom":"5rem","right":"var:preset|spacing|edge-space"}}},"backgroundColor":"charcoal-2","textColor":"white","layout":{"type":"constrained","wideSize":"1320px"}} -->
+<div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" style="padding-top:5rem;padding-left:var(--wp--preset--spacing--edge-space);padding-bottom:5rem;padding-right:var(--wp--preset--spacing--edge-space)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"5rem","left":"5rem"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"fontSize":"heading-1"} -->
 <h2 class="has-heading-1-font-size">Get more help</h2>
 <!-- /wp:heading -->
 
@@ -19,8 +18,8 @@
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
-<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:list {"className":"is-style-links-list"} -->
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list {"className":"is-style-links-list"} -->
 <ul class="is-style-links-list"><!-- wp:list-item -->
 <li><a href="#">Ask questions in the support forums ↗</a></li>
 <!-- /wp:list-item -->
