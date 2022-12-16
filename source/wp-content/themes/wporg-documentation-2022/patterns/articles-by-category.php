01021714a7601a6ba86389faf15576bd832a572b
Kelly Dwan <ryelle@users.noreply.github.com>
2022-12-16 16:07:52 -0500

    Update spacing across templates to be more consistent
    Fixes #20

--- source/wp-content/themes/wporg-documentation-2022/patterns/articles-by-category.php
+++ source/wp-content/themes/wporg-documentation-2022/patterns/articles-by-category.php
@@ -6,20 +6,20 @@
  */
 
 ?>
-<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"default"}} -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|30"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|light-grey-1","style":"solid","width":"1px"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group" style="border-bottom-color:var(--wp--preset--color--light-grey-1);border-bottom-style:solid;border-bottom-width:1px"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-<h2 style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Installation</h2>
+<div class="wp-block-group" style="border-bottom-color:var(--wp--preset--color--light-grey-1);border-bottom-style:solid;border-bottom-width:1px"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"bottom":"var:preset|spacing|10"}}}} -->
+<h2 class="wp-block-heading" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-bottom:var(--wp--preset--spacing--10)">Security</h2>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
 
-<!-- wp:query {"queryId":29,"query":{"perPage":"10","pages":0,"offset":0,"postType":"helphub_article","order":"asc","orderBy":"title","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"taxQuery":{"category":[19]}},"displayLayout":{"type":"list","columns":2}} -->
+<!-- wp:query {"query":{"perPage":10,"pages":0,"offset":0,"postType":"helphub_article","order":"asc","orderBy":"title","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"taxQuery":{"category":[32]}},"displayLayout":{"type":"list","columns":2}} -->
 <div class="wp-block-query"><!-- wp:post-template -->
-<!-- wp:post-title {"level":3,"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|blueberry-1"}}}},"textColor":"blueberry-1","fontSize":"normal","fontFamily":"inter"} /-->
+<!-- wp:post-title {"level":3,"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|blueberry-1"}}},"spacing":{"margin":{"bottom":"var:preset|spacing|20","top":"0"}}},"textColor":"blueberry-1","fontSize":"normal","fontFamily":"inter"} /-->
 <!-- /wp:post-template --></div>
 <!-- /wp:query -->
 
 <!-- wp:paragraph -->
-<p><a href="https://wordpress.org/support/category/installation/">Show all articles in Installation</a></p>
+<p><a href="#">Show all articles in Security</a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
