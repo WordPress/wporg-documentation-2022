279dc9b19966900bcc93d8e71bf6f717fe3aecf3
Kelly Dwan <ryelle@users.noreply.github.com>
2023-01-10 13:30:43 -0500

    mu-plugins: Rename the site's mu-plugin to reflect the real site
    This also breaks the sync between `site-support.php` and this repo, the source for that file is now dotorg.svn.
    
    See [19817-dotorg].

---
+++ source/wp-content/mu-plugins/site-documentation.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This mu-plugin is loaded only on the wordpress.org/documentation/ site.
+ *
+ * Note: This file is synced from github, make edits in this file:
+ * https://github.com/WordPress/wporg-documentation-2022/blob/trunk/source/wp-content/mu-plugins/site-documentation.php
+ * And sync the changes with ./bin/sync/wporg-documentation.sh.
+ */
+
+namespace WordPressdotorg\Documentation_2022\MU_Plugin;
