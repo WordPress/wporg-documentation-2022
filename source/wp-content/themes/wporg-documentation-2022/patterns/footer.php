d81a9fdc7abffe17c0059befafc2d03381137e04
Kelly Dwan <ryelle@users.noreply.github.com>
2023-01-16 13:43:51 -0500

    Footer: Vertically center the "Get help" banner content

--- source/wp-content/themes/wporg-documentation-2022/patterns/footer.php
+++ source/wp-content/themes/wporg-documentation-2022/patterns/footer.php
@@ -8,8 +8,8 @@
 ?>
 <!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"5rem","left":"var:preset|spacing|edge-space","bottom":"5rem","right":"var:preset|spacing|edge-space"}}},"backgroundColor":"charcoal-2","textColor":"white","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" style="padding-top:5rem;padding-left:var(--wp--preset--spacing--edge-space);padding-bottom:5rem;padding-right:var(--wp--preset--spacing--edge-space)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"5rem","left":"5rem"}}}} -->
-<div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"fontSize":"heading-1"} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"fontSize":"heading-1"} -->
 <h2 class="has-heading-1-font-size">Get more help</h2>
 <!-- /wp:heading -->
 
@@ -18,8 +18,8 @@
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:list {"className":"is-style-links-list"} -->
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:list {"className":"is-style-links-list"} -->
 <ul class="is-style-links-list"><!-- wp:list-item -->
 <li><a href="https://wordpress.org/support/forums/">Ask questions in the support forums ↗</a></li>
 <!-- /wp:list-item -->
