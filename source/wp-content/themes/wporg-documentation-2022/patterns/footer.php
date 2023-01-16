9e5969ed408be77518f97f8195826bcb1c7c4444
Kelly Dwan <ryelle@users.noreply.github.com>
2023-01-16 13:03:30 -0500

    Local Footer: Update width of "Get more help" banner.
    Fixes #39

--- source/wp-content/themes/wporg-documentation-2022/patterns/footer.php
+++ source/wp-content/themes/wporg-documentation-2022/patterns/footer.php
@@ -6,9 +6,9 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"5rem","left":"var:preset|spacing|edge-space","bottom":"5rem","right":"var:preset|spacing|edge-space"}}},"backgroundColor":"charcoal-2","textColor":"white","layout":{"type":"constrained","wideSize":"1320px"}} -->
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"5rem","left":"var:preset|spacing|edge-space","bottom":"5rem","right":"var:preset|spacing|edge-space"}}},"backgroundColor":"charcoal-2","textColor":"white","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" style="padding-top:5rem;padding-left:var(--wp--preset--spacing--edge-space);padding-bottom:5rem;padding-right:var(--wp--preset--spacing--edge-space)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"5rem","left":"5rem"}}}} -->
-<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading {"fontSize":"heading-1"} -->
 <h2 class="has-heading-1-font-size">Get more help</h2>
 <!-- /wp:heading -->
