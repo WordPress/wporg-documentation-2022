9aaf194fa4fb45cd13a94bdabfc41b54f1860f64
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-09-05 14:26:03 +1200

    Remove breadcrumbs and update nav links

--- source/wp-content/themes/wporg-documentation-2022/patterns/header.php
+++ source/wp-content/themes/wporg-documentation-2022/patterns/header.php
@@ -19,19 +19,12 @@
 		
 		<!-- wp:navigation {"textColor":"white","backgroundColor":"charcoal-2","className":"is-style-dropdown-on-mobile","style":{"spacing":{"blockGap":"24px"}},"fontSize":"small"} -->
 
-			<!-- wp:navigation-link {"label":"Forums","url":"https://wordpress.org/support/forums/","kind":"custom","isTopLevelLink":true} /-->
-			<!-- wp:navigation-link {"label":"Developers","type":"custom","url":"https://developer.wordpress.org/","kind":"custom","isTopLevelLink":true} /-->
-			<!-- wp:navigation-link {"label":"Get Involved","type":"custom","url":"https://make.wordpress.org/docs/","kind":"custom","isTopLevelLink":true} /-->
+			<!-- wp:navigation-link {"label":"WordPress Overview","url":"https://wordpress.org/documentation/overview/","kind":"custom","isTopLevelLink":true} /-->
+			<!-- wp:navigation-link {"label":"Technical Guides","type":"custom","url":"https://wordpress.org/documentation/technical-guides/","kind":"custom","isTopLevelLink":true} /-->
+			<!-- wp:navigation-link {"label":"Support Guides","type":"custom","url":"https://wordpress.org/documentation/support-guides/","kind":"custom","isTopLevelLink":true} /-->
+			<!-- wp:navigation-link {"label":"Customization","type":"custom","url":"https://wordpress.org/documentation/customization/","kind":"custom","isTopLevelLink":true} /-->
 
 		<!-- /wp:navigation -->
-
-	</div>
-	<!-- /wp:group -->
-
-	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"18px","bottom":"18px","left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}}},"backgroundColor":"white","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:18px;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:18px;padding-left:var(--wp--preset--spacing--edge-space)">
-		
-		<!-- wp:wporg/site-breadcrumbs {"fontSize":"small"} /-->
 
 	</div>
 	<!-- /wp:group -->
