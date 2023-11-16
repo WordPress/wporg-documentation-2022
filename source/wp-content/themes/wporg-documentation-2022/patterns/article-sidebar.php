4c0568f50293d56b51cecbcf4135deed07e6fddc
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-11-17 10:58:23 +1300

    Fix spacing around collapsed Table of Contents
    See https://github.com/WordPress/wporg-mu-plugins/issues/491

--- source/wp-content/themes/wporg-documentation-2022/patterns/article-sidebar.php
+++ source/wp-content/themes/wporg-documentation-2022/patterns/article-sidebar.php
@@ -6,7 +6,7 @@
  */
 
 ?>
-<!-- wp:wporg/sidebar-container -->
+<!-- wp:wporg/sidebar-container {"style":{"spacing":{"margin":{"bottom":"40px"}}}} -->
 	<!-- wp:template-part {"slug":"search","theme":"wporg-documentation-2022"} /-->
 
 	<!-- wp:wporg/table-of-contents /-->
