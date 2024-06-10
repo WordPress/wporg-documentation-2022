61013c1f57529de002f10cc4166eab2c21cf8c28
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-06-10 21:13:25 +1200

    Move search bar to top left of all pages

--- source/wp-content/themes/wporg-documentation-2022/patterns/article-sidebar.php
+++ source/wp-content/themes/wporg-documentation-2022/patterns/article-sidebar.php
@@ -7,7 +7,5 @@
 
 ?>
 <!-- wp:wporg/sidebar-container {"style":{"spacing":{"margin":{"bottom":"40px"}}}} -->
-	<!-- wp:template-part {"slug":"search","theme":"wporg-documentation-2022"} /-->
-
 	<!-- wp:wporg/table-of-contents /-->
 <!-- /wp:wporg/sidebar-container -->
